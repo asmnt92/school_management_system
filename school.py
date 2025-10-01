f0cab22bd251bcc1dbebdd72b5d82213ab64ba23
awal <awal.aaraf.92@gmail.com>
2025-10-01 23:06:02 +0600

    first commit

---
+++ school.py
@@ -0,0 +1,28 @@
+class School:
+    
+    name=''
+    address=''
+    teachers={}
+    classrooms={}
+
+    def add_classroom(classroom):
+        pass
+
+    def  add_teacher(subject, teacher):
+        pass
+
+    def student_admission(student):
+        pass
+    
+    def calculate_grade(marks):
+        pass
+
+    def grade_to_value(grade):
+        pass
+
+    def value_to_grade(value):
+        pass
+
+
+    def __repr__(self):
+        pass
---
+++ subject.py
@@ -0,0 +1,11 @@
+class Subject:
+    
+    def __init__(self,name,teacher,max_mark,pass_mark):
+        self.name=name
+        self.teacher=teacher
+        self.max_marks=max_mark
+        self.pass_mark=pass_mark
+
+
+    def exam(students):
+        pass
